d57f1dc604bb2944270cfbf95177426bcba243fb
Maxim Frolov <buterbrodhs@gmail.com>
2020-10-02 21:08:41 +0400

    Добавлена базовая верстка приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import "./main.global.css";
+
+import { Content } from "./Content";
 import { Header } from "./Header";
+import { Layout } from "./Layout";
 
 export function App() {
   return (
     <div>
-      <Header />
-      <div>Content</div>
+      <Layout>
+        <Header />
+        <Content>Содержимое html-тега main</Content>
+      </Layout>
     </div>
   );
 }
